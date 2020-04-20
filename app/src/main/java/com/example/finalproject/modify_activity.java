a34d329705030437e1500cc57c4629e88d35c970
chuongdavid <chuongddavid@gmail.com>
2020-04-20 20:04:29 +0700

    Modify thanh cong

---
+++ app/src/main/java/com/example/finalproject/modify_activity.java
@@ -0,0 +1,13 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modify_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
